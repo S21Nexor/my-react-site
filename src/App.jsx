6037e1cf90e70059176a4c6527254facad80ffcb
S21Nexor <saggar676@gmail.com>
2026-05-06 16:20:40 +0300

    Fix import paths in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PublicRegistration from "./pages/PublicRegistration.jsx";
-import AdminLogin from "./pages/AdminLogin.jsx";
-import AdminDashboard from "./pages/AdminDashboard.jsx";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+import PublicRegistration from "./pages/PublicRegistration";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<PublicRegistration />} />
+
         <Route path="/admin" element={<AdminLogin />} />
+
         <Route
           path="/admin/dashboard"
           element={
@@ -18,6 +22,7 @@ export default function App() {
             </ProtectedRoute>
           }
         />
+
       </Routes>
     </BrowserRouter>
   );
